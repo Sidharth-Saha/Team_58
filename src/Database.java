ad712ac7403c6949896b3e344cafc6b416823272
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2021-11-17 22:25:09 -0700

    Add files via upload

--- src/Database.java
+++ src/Database.java
@@ -27,5 +27,32 @@ public class Database implements Serializable {
         objectStream.writeObject(database);
     }
 }
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.SelectionMode;
+import javafx.stage.Stage;
+public class Database extends User {
+	public void start(Stage primaryStage)throws Exception {
+		primaryStage.setTitle("Database");
+		ListView<Message> listView = new ListView<>();
+		listView.getItems().add("Patient");
 
+		Button button = new Button("Patient");
+
+		button.setOnAction(event -> {
+            ObservableList selectedIndices = ListView.getSelectionModel().getSelectedItem();
+
+		VBox vBox = new VBox(listView, button);
+
+		        Scene scene = new Scene(vBox, 300, 120);
+		        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+	public static void main(String[] args) {
+	        Application.launch(args);
+	    }
 }
